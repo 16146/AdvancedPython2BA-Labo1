dc2d45ad9665434c3d215cfde3641e866a8d5e35
16146 <36259631+16146@users.noreply.github.com>
2018-02-08 10:17:48 +0100

    Add unit test for utils module

---
+++ test_utils.py
@@ -0,0 +1,21 @@
+# test_utils.py
+# Author: Sébastien Combéfis
+# Version: February 8, 2018
+
+import unittest
+import utils
+
+class TestUtils(unittest.TestCase):
+    def test_fact(self):
+        self.assertEqual(utils.fact(5),120)
+    
+    def test_roots(self):
+        self.assertEqual(utils.roots(5,1,1),4)
+    
+    def test_integrate(self):
+        self.assertEqual(utils.integrate(0,5,6),10)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
+    runner = unittest.TextTestRunner()
+    exit(not runner.run(suite).wasSuccessful())
