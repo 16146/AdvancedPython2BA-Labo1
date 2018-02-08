9110d26413a1acc21ea2a262375ecb3e65a28826
16146 <16146@ecam.be>
2018-02-08 11:49:28 +0100

    Add more tests

--- test_utils.py
+++ test_utils.py
@@ -11,6 +11,8 @@ class TestUtils(unittest.TestCase):
     
     def test_roots(self):
         self.assertEqual(utils.roots(5,1,1),"Racine négative")
+        self.assertEqual(utils.roots(1,5,1),>0)
+        
     
     def test_integrate(self):
         self.assertEqual(utils.integrate(0,5,6),"Réussi")
